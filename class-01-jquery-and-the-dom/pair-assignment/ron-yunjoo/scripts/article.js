710c435377b137a23e9c931872d53a369f947d76
YUN JOO KIM <moniqueyj282@hotmail.com>
2016-01-07 18:54:42 -0800

    1st todo

--- class-01-jquery-and-the-dom/pair-assignment/ron-yunjoo/scripts/article.js
+++ class-01-jquery-and-the-dom/pair-assignment/ron-yunjoo/scripts/article.js
@@ -4,19 +4,24 @@ function Article (opts) {
   // TODO: Use the js object passed in to complete this contructor function:
   // Save ALL the properties of `opts` into `this`.
   this.author = opts.author;
+  this.category = opts.category;
+  this.authorUrl = opts.uthorUrl;
+  this.title = opts.title;
+  this.body = opts.body;
+  this.publishedOn = opts.publishedOn;
 }
 
 Article.prototype.toHtml = function() {
   var $newArticle = $('article.template').clone();
 
   $newArticle.data('category', this.category);
-
+  
   // TODO: Use jQuery to fill in the template with properties
   // from this particular Article instance. We need to fill in:
   // the author name and url, the article title and body, and the
   // publication date.
-
   // Include the publication date as a 'title' attribute to show on hover:
+
   $newArticle.find('time[pubdate]').attr('title', this.publishedOn)
 
   // Display the date as a relative number of "days ago":
