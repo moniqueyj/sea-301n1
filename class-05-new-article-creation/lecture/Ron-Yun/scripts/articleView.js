d508e87c7aeee27e11d4e00855aed749b6132706
YUN JOO KIM <moniqueyj282@hotmail.com>
2016-02-13 07:10:08 -0800

    articleView.js file

--- class-05-new-article-creation/lecture/Ron-Yun/scripts/articleView.js
+++ class-05-new-article-creation/lecture/Ron-Yun/scripts/articleView.js
@@ -63,6 +63,12 @@ articleView.setTeasers = function() {
 };
 
 articleView.initNewArticlePage = function() {
+   $(".tab-content").show();
+   $("#export-field").hide();
+   $("#article-json").on("focus", function(){
+     this.select();
+   });
+   $("#new-form").on("change", "input, textarea", articleView.create);
   // TODO: Ensure the main .tab-content area is revealed. We might add more tabs later.
 
   // TODO: The new articles we create will be copy/pasted into our source data file.
@@ -74,16 +80,27 @@ articleView.initNewArticlePage = function() {
 articleView.create = function() {
   // TODO: Set up a var to hold the new article we are creating.
   // Clear out the #articles element, so we can put in the updated preview
-
+  var article;
+  $("#articles").empty();
   // TODO: Instantiate an article based on what's in the form fields:
-
+  article = new Article({
+    title: $('#article-title').val(),
+    author: $('#article-author').val(),
+    authorUrl: $('#article-author-Url').val(),
+    category: $('#article-category').val(),
+    body: $('#article-body').val(),
+    publishedOn: $('#article-published: checked').length ? util.today() : null
+  });
   // TODO: Use our interface to the Handblebars template to put this new article into the DOM:
-
+$("#articles").append(article.toHtml());
   // TODO: Activate the highlighting of any code blocks:
-
+$('pre code').each(function(i, block){
+  hljs.highlightBlock(block);
+});
+$("export-field").show();
+$('#article-jason').val(JSON.stringify(article) + ',');
   // TODO: Export the new article as JSON, so it's ready to copy/paste into blogArticles.js:
 };
-
 
 articleView.initIndexPage = function() {
   articleView.populateFilters();
