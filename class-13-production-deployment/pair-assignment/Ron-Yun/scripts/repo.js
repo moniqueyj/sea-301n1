d1e8809f73bd44bedf2558fdaca5be94ec140178
YUN JOO KIM <moniqueyj282@hotmail.com>
2016-02-23 12:25:27 -0800

    update index and repo.js

--- class-13-production-deployment/pair-assignment/Ron-Yun/scripts/repo.js
+++ class-13-production-deployment/pair-assignment/Ron-Yun/scripts/repo.js
@@ -5,15 +5,11 @@
 
   // TODO: Refactor this ajax call into a get request to the proxy end point provided by server.js.
   repos.requestRepos = function(callback) {
-    $.ajax({
-      url: 'https://api.github.com/users/brookr/repos' +
+    $.get('github/users/brookr/repos' +
             '?per_page=100' +
-            '&sort=updated',
-      type: 'GET',
-      headers: { 'Authorization': 'token ' + githubToken },
-      success: function(data, message, xhr) {
+            '&sort=updated')
+      .done(function(data, message, xhr) {
         repos.all = data;
-      }
     }).done(callback);
   };
 
